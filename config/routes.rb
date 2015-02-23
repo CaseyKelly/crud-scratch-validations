c11df9ad0cda17f22ac1b01d35dbd02c93366c71
CaseyKelly <casey.kelly@colorado.edu>
2015-02-22 20:53:35 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :status_updates
+  get '/like' => 'status_updates#like'
+
+ root 'status_updates#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
